41f210254e1713fef33bcb4f101e1155461ef7b4
brianartea <bri@expertdesign.cc>
2021-04-26 11:03:00 -0400

    Finished Disney Plus Clone

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDvVbEGcRKPSFxddWD0R3yLUSQ9EpBYsxA",
-  authDomain: "disney-clone-eaa38.firebaseapp.com",
-  projectId: "disney-clone-eaa38",
-  storageBucket: "disney-clone-eaa38.appspot.com",
-  messagingSenderId: "615281038395",
-  appId: "1:615281038395:web:26efe63afb5a55a872781e",
-};
+//firebase config file
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
